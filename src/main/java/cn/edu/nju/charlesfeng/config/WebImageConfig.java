187aac8e2b9afdd043b4930542d7f31c2c8bc919
151250040 <151250040@smail.nju.edu.cn>
2018-06-26 15:14:06 +0800

    fix() 修复跨域报错的问题

--- src/main/java/cn/edu/nju/charlesfeng/config/WebImageConfig.java
+++ src/main/java/cn/edu/nju/charlesfeng/config/WebImageConfig.java
@@ -1,11 +1,25 @@
 package cn.edu.nju.charlesfeng.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
+import static org.springframework.web.cors.CorsConfiguration.ALL;
+
 @Configuration
 public class WebImageConfig extends WebMvcConfigurationSupport {
+
+
+    @Override
+    protected void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(ALL)
+                .allowedMethods(ALL)
+                .allowedHeaders(ALL)
+                .allowCredentials(true);
+        super.addCorsMappings(registry);
+    }
 
     /**
      * Override this method to add resource handlers for serving static resources.
@@ -15,7 +29,7 @@ public class WebImageConfig extends WebMvcConfigurationSupport {
      */
     @Override
     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/image/**").addResourceLocations("file:/F:/image/");
+        registry.addResourceHandler("/image/**").addResourceLocations("file:/D:/java/image/");
         super.addResourceHandlers(registry);
     }
 }
