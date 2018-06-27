6fea332e6fcccc09be304aae38874ee49a7885ac
151250040 <151250040@smail.nju.edu.cn>
2018-06-27 11:37:27 +0800

    修改不能star的bug

--- src/main/java/cn/edu/nju/charlesfeng/model/id/ProgramID.java
+++ src/main/java/cn/edu/nju/charlesfeng/model/id/ProgramID.java
@@ -21,6 +21,9 @@ public class ProgramID implements Serializable {
     @Column(name = "start_time")
     private LocalDateTime startTime;
 
+    public ProgramID() {
+    }
+
     public int getVenueID() {
         return venueID;
     }
