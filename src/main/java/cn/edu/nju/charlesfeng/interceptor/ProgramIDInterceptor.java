a3b8322308679b1246d5f5377639d9fbbd0d040c
ByronDong <2104869365@qq.com>
2018-06-29 14:43:06 +0800

    提供订单按时间倒序排列， 拦截器规范一下

--- src/main/java/cn/edu/nju/charlesfeng/interceptor/ProgramIDInterceptor.java
+++ src/main/java/cn/edu/nju/charlesfeng/interceptor/ProgramIDInterceptor.java
@@ -1,7 +1,6 @@
 package cn.edu.nju.charlesfeng.interceptor;
 
-import cn.edu.nju.charlesfeng.util.enums.RequestReturnObjectState;
-import cn.edu.nju.charlesfeng.util.helper.RequestReturnObject;
+import com.alibaba.fastjson.JSONObject;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
@@ -9,6 +8,9 @@ import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+/**
+ * 用于拦截需要传输program_id的接口
+ */
 @Component
 public class ProgramIDInterceptor implements HandlerInterceptor {
 
@@ -16,11 +18,13 @@ public class ProgramIDInterceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String id = request.getParameter("program_id");
         String ids[] = id.split("-");
-        if (!id.contains("-") || ids.length != 2) {
+        if (!id.contains("-") || ids.length != 2) { //不符合系统指定id格式
+            JSONObject jsonObject = new JSONObject();
             response.setStatus(200);
             response.setCharacterEncoding("UTF-8");
             response.setContentType("application/json; charset=utf-8");
-            response.getWriter().write("wrong_program_id");
+            jsonObject.put("programIdInterceptorInfo", "WRONG_PROGRAM_ID");
+            response.getWriter().write(jsonObject.toJSONString());
             return false;
         }
         return true;
