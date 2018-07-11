21f34e416a9041180b017685f3630ba6e82ca766
Shenmiu <gcm971016@gmail.com>
2018-07-11 10:57:09 +0800

    Revert "跨域訪問"
    This reverts commit f16b62e

--- src/main/java/cn/edu/nju/charlesfeng/config/WebMvcConfig.java
+++ src/main/java/cn/edu/nju/charlesfeng/config/WebMvcConfig.java
@@ -1,5 +1,6 @@
 package cn.edu.nju.charlesfeng.config;
 
+import cn.edu.nju.charlesfeng.interceptor.CorsInterceptor;
 import cn.edu.nju.charlesfeng.interceptor.ProgramIDInterceptor;
 import cn.edu.nju.charlesfeng.interceptor.UserInterceptor;
 import com.alibaba.fastjson.serializer.SerializerFeature;
@@ -39,7 +40,7 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
     @Override
     protected void addInterceptors(InterceptorRegistry registry) {
         //Cors
-//        registry.addInterceptor(new CorsInterceptor()).addPathPatterns("/**");
+        registry.addInterceptor(new CorsInterceptor()).addPathPatterns("/**");
         //UserInterceptor
         registry.addInterceptor(new UserInterceptor())
                 //member
---
+++ src/main/java/cn/edu/nju/charlesfeng/interceptor/CorsInterceptor.java
@@ -0,0 +1,35 @@
+package cn.edu.nju.charlesfeng.interceptor;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 拦截器的跨域访问
+ */
+@Component
+public class CorsInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String origin = request.getHeader("Origin");
+        response.setHeader("Access-Control-Allow-Origin", origin);
+        response.setHeader("Access-Control-Allow-Methods", "*");
+        response.setHeader("Access-Control-Allow-Headers", "Origin,Content-Type,Accept,token,X-Requested-With,X-Token");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+    }
+}
